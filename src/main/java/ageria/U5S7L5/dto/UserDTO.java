20661ef27c24e4d0776b45e9c9ade5f9450d38bb
Andrea <ageria20@gmail.com>
2024-09-20 10:25:11 +0200

    adding user class

---
+++ src/main/java/ageria/U5S7L5/dto/UserDTO.java
@@ -0,0 +1,4 @@
+package ageria.U5S7L5.dto;
+
+public record UserDTO() {
+}
--- src/main/java/ageria/U5S7L5/entities/User.java
+++ src/main/java/ageria/U5S7L5/entities/User.java
@@ -1,7 +1,17 @@
 package ageria.U5S7L5.entities;
 
 
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+
+    @Id
 }
